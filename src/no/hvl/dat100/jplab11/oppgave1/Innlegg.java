707e110e34a7dbb66b88fd8c5c062a2c363d5349
h602112 <markusnedrevold@live.no>
2021-11-02 06:44:53 +0100

    Ferdig med Innlegg klassen

--- src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
+++ src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
@@ -3,6 +3,10 @@ package no.hvl.dat100.jplab11.oppgave1;
 import no.hvl.dat100.jplab11.common.TODO;
 
 public abstract class Innlegg {
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
 	
 	// TODO - deklarering av objektvariable
 	
@@ -11,66 +15,75 @@ public abstract class Innlegg {
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = 0;
 
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		// TODO
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = likes;
 
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
-	}
-	
-	public String getBruker() {
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-	}
-
-	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-
-	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setDato(String dato) {
-		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBruker() {
+		return bruker;
+	}
+
+	public void setBruker(String bruker) {
+		this.bruker = bruker;
+	}
+
+	public String getDato() {
+		return dato;
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
 	}
 
 	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return likes;
 	}
-	
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
 	public void doLike () {
-		throw new UnsupportedOperationException(TODO.method());
+		this.likes += 1;
 	}
 	
 	public boolean erLik(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		return innlegg.getId() == this.id;
 
 	}
 	
 	@Override
 	public String toString() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return this.id +  "\n" + this.bruker + "\n" + "23-10\n" + this.likes + "\n";
 				
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 				
 	}
