ad7b1bad27fab37a485689cc4bd45be12bd5ad84
h602112 <markusnedrevold@live.no>
2021-11-02 07:44:43 +0100

    Ferdig med bilde og tekst klassene.

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -3,29 +3,33 @@ package no.hvl.dat100.jplab11.oppgave2;
 import no.hvl.dat100.jplab11.common.TODO;
 
 public class Bilde extends Tekst {
+	private String url;
 
 	// TODO - objekt variable
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id, bruker, dato, tekst);
+		this.url = url;
+
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id, bruker, dato, likes, tekst);
+		this.url = url;
 	}
-	
-	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
 
+	public String getUrl() {
+		return url;
 	}
 
 	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.url = url;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+		return "BILDE" + "\n" + this.getId() + "\n" + this.getBruker() + "\n" + "23-10" +
+				"\n" + this.getLikes() + "\n" + "et bilde" + "\n" + this.url + "\n";
 
 	}
 
--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -4,6 +4,7 @@ import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
+	private String tekst;
 
 	// TODO: objektvariable 
 	
@@ -12,26 +13,28 @@ public class Tekst extends Innlegg {
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		super(id, bruker, dato);
+		this.tekst = tekst;
+
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
 	}
-	
-	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
 
+	public String getTekst() {
+		return tekst;
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.tekst = tekst;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return "TEKST" + "\n" + this.getId() + "\n" + this.getBruker() + "\n" + "23-10" + "\n" + this.getLikes()
+				+ "\n" + this.tekst + "\n";
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
